5b3f652a13f73593784e986352812b2cf61e9ef6
sharkba1t <87448201+sharkba1t@users.noreply.github.com>
2022-01-21 16:48:08 -0500

    implement input log

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,44 +3,87 @@ import './App.css';
 import Display from '../Display/Display';
 import Keypad from '../Keypad/Keypad';
 
+const operationKey = {
+  "=": 6,
+  "+": 1,
+  "-": 2,
+  "*": 3,
+  "/": 4,
+  "%": 5,
+  '±': 7,
+}
+
+const operations = Object.keys(operationKey)
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentInput: '0',
-      input: []
+      currentInput: 0,
+      previousInput: 0,
+      input: [],
+      operation: '',
+      isDecimal: false,
+      isOperation: false
     }
     this.changeCurrentInput = this.changeCurrentInput.bind(this);
-    this.mathOperation = this.mathOperation.bind(this);
+    this.operationDisplay = this.operationDisplay.bind(this);
   }
 
   changeCurrentInput(number) {
     let inputDisplay = this.state.currentInput;
-    if (number === "c"){
-      inputDisplay = '0';
-    }  else if (this.state.currentInput === '0') {
+    let allInputs = this.state.input;
+    if (number === "C"){
+      inputDisplay = 0;
+      allInputs = []
+      this.setState({
+        operation: ''
+      })
+    } else if (operations.findIndex(element => element === number) > -1) {
+       if (this.state.isOperation && number !== '='){
+        allInputs[allInputs.length - 1] = number
+        console.log(allInputs)
+       } else {
+        this.operationDisplay(operations.findIndex(element => element === number));
+        allInputs.push(inputDisplay);
+        allInputs.push(number);
+        this.setState({
+          isOperation: true
+        })
+      }
+    } else if (this.state.currentInput === 0 || this.state.isOperation) {
       inputDisplay = number;
-
+      this.setState({
+        isOperation: false
+      })
+    } else {
+        inputDisplay += number;
     }
-    else {
-    inputDisplay += number;
-    }
+    console.log(allInputs)
     this.setState(
       {
-        currentInput: inputDisplay
+        currentInput: inputDisplay,
+        input: allInputs
       })
   }
 
-  mathOperation() {
-    
+  operationDisplay(index) {
+    if (operations[index] !== '='){
+    this.setState({
+      operation: operations[index]
+    }) 
+  } else {
+    this.setState({
+      operation: ''
+    }) 
+  }
   }
 
   render(){
     return (
       <div className='app'>
         <div className='mainArea'>
-          <Display value= {this.state.currentInput} />
+          <Display value= {this.state.currentInput} operationKey={this.state.operation}/>
           <Keypad onClick={this.changeCurrentInput} />
          </div>
       </div>

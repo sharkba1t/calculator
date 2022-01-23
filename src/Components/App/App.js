a7bba49eb532f37b461032b0c569112f5b93e6c1
sharkba1t <87448201+sharkba1t@users.noreply.github.com>
2022-01-22 21:47:46 -0500

    calculator is now functional, kind of

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,11 +4,11 @@ import Display from '../Display/Display';
 import Keypad from '../Keypad/Keypad';
 
 const operationKey = {
-  "=": () => {},
+  "=": (first, second, operation) => {return this[operation](first, second)},
   "+": function(first, second) {return first + second},
-  "-": function(first, second) {return first + second},
-  "*": function(first, second) {return first + second},
-  "/": function(first, second) {return first + second},
+  "-": function(first, second) {return first - second},
+  "*": function(first, second) {return first * second},
+  "/": function(first, second) {return first / second},
   "%": function(number) {return number / 100},
   '±': function(number) {return -number},
 }
@@ -24,70 +24,112 @@ class App extends React.Component {
       input: [],
       operation: '',
       isDecimal: false,
-      isOperation: false
+      isFirstNumber: true,
+      isPreviousInput: false
     }
     this.changeCurrentInput = this.changeCurrentInput.bind(this);
     this.operationDisplay = this.operationDisplay.bind(this);
+    this.resetDisplay = this.resetDisplay.bind(this);
   }
 
   changeCurrentInput(input) {
     let inputDisplay = this.state.currentInput;
     let allInputs = this.state.input;
+    let previousInput;
     if (input === "C"){
       inputDisplay = 0;
       allInputs = []
-      this.setState({
-        operation: '',
-        isDecimal: false
-      })
+      this.resetDisplay()
     } else if (operations.findIndex(element => element === input) > -1) {
-       if (this.state.isOperation && input !== '='){
-        allInputs[allInputs.length - 1] = input
-        console.log(allInputs)
-       } else {
-        allInputs.push(inputDisplay);
-        if (['+','-','*', '/'].indexOf(input) === -1) {
-          this.setState({isDecimal: false, isOperation: true})
+        this.setState({currentInput: inputDisplay})
+        if (!this.state.isPreviousInput) {
+          previousInput = this.state.currentInput;
+          console.log('this happened.')
+          this.setState({
+            previousInput: previousInput,
+            currentInput: inputDisplay
+          })
         }
-        inputDisplay = this.operationDisplay(input);
+        allInputs.push(inputDisplay); 
+        if (['+','-','*', '/'].indexOf(input) !== -1) {
+          this.setState({isDecimal: false, isPreviousInput: true})
+          allInputs.push(input);
+        } 
+        inputDisplay = this.operationDisplay(input) || inputDisplay;
       }
-    } else if (input === '.'){
+      else if (input === '.'){
       if (!this.state.isDecimal) {
         inputDisplay += input
         this.setState({
           isDecimal: true
         })
-    }} else if (this.state.currentInput === 0 || this.state.isOperation) {
+    }} else if (this.state.isFirstNumber) {
       inputDisplay = input;
       this.setState({
-        isOperation: false
+        isFirstNumber: false,
+        currentInput: inputDisplay,
+        input: allInputs
       })
     } else {
         inputDisplay += input;
+        this.setState(
+          {
+            currentInput: inputDisplay,
+            input: allInputs
+          })
     }
     console.log(allInputs)
     this.setState(
       {
         currentInput: inputDisplay,
-        input: allInputs
       })
   }
 
+  resetDisplay() {
+    this.setState({
+      operation: '',
+      currentInput: 0,
+      previousInput: 0,
+      isDecimal: false,
+      isPreviousInput: false,
+      isFirstNumber: true
+    })
+  }
 
   operationDisplay(input) {
     let newNumber;
-    if (['=','%','±'].indexOf(input) === -1){
+    let first = parseFloat(this.state.previousInput);
+    let second = parseFloat(this.state.currentInput);
+    let inputs = this.state.input
+    if (input === '='){
+      console.log(inputs)
+      newNumber = operationKey[inputs[1]](parseFloat(inputs[0]),parseFloat(inputs[2]))
+      this.setState({input: [newNumber] })
+      return newNumber
+    } else if (['=','%','±'].indexOf(input) === -1){
+      if (!this.state.isPreviousInput){
+        newNumber = this.state.currentInput;
     this.setState({
-      operation: input
+      operation: input,
+      currentInput: 0,
+      isFirstNumber: true,
+      isPreviousInput: true
     }) 
   } else {
+      newNumber = operationKey[input](first, second);
+      this.setState({
+        operation: '',
+        previousInput: newNumber,
+        currentInput: 0,
+        isPreviousInput: false,
+        input: [newNumber]
+      })
+      return newNumber;
+    }
+  } else {
     newNumber = operationKey[input](this.state.currentInput)
-    console.log(newNumber)
-    this.setState({
-      operation: ''
-    }) 
+    return newNumber
   }
-  return newNumber
   }
 
   render(){
